6bce0ee85e0fc52af77143691a9661ffd0d44a74
funky <mrfunkyman@pm.me>
2021-10-17 16:31:40 +0200

    updated setup and main loop

--- scale.ino
+++ scale.ino
@@ -1,4 +1,4 @@
-#include "HX711.h"  
+#include "HX711.h"
 #include <SPI.h>
 #include <EthernetENC.h>
 #include <PubSubClient.h>
@@ -6,10 +6,19 @@
 
 void setup() {
   // put your setup code here, to run once:
-  
+  Serial.begin(115200);
+  Serial.println("Scale arduino");
+
+  hx711Setup();
+  Serial.println("Bin scale setup ready");
+
+
+  setupMqttClient();
+  Serial.println("Mqtt client Ready.");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  hx711Loop();
+  mqttLoop();
 }
